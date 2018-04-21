a10cc309e2359b4730e537663b03ca858368abad
toonae <sup.ta1909@gmail.com>
2018-04-21 20:48:28 +0700

    ใหม่สุดๆๆ+เพิ่มเพลงลูกทุ่ง+เพิ่มเพลงสากล++

--- app/src/main/java/com/example/computer/uknowmusic/homeActivity.java
+++ app/src/main/java/com/example/computer/uknowmusic/homeActivity.java
@@ -8,31 +8,48 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class homeActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4;
 
-    Button startbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
-        startbtn=findViewById(R.id.b1);
-        startbtn=findViewById(R.id.b2);
-        startbtn=findViewById(R.id.b3);
-        startbtn=findViewById(R.id.b4);
-
-        startbtn.setOnClickListener(new View.OnClickListener() {
+        b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent starIntent = new Intent(homeActivity.this, GameActivity.class);
-                startActivity(starIntent);
+                Intent b1 = new Intent(homeActivity.this,GameActivity.class);
+                startActivity(b1);
             }
         });
 
+        b2=(Button)findViewById(R.id.b2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b2 = new Intent(homeActivity.this,game2Activity.class);
+                startActivity(b2);
+            }
+        });
 
+        b3=(Button)findViewById(R.id.b3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b3 = new Intent(homeActivity.this,game3Activity.class);
+                startActivity(b3);
+            }
+        });
 
-
-
+        b4=(Button)findViewById(R.id.b4);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b4 = new Intent(homeActivity.this,game1Activity.class);
+                startActivity(b4);
+            }
+        });
     }
 }
